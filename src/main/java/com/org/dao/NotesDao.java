4969d93c9762c23e8f649b745d6f17d1571acacf
WiseM0nk <satyaboy172@gmail.com>
2024-10-23 11:22:31 +0530

    uploaded modification

--- src/main/java/com/org/dao/NotesDao.java
+++ src/main/java/com/org/dao/NotesDao.java
@@ -1,23 +1,51 @@
 package com.org.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.org.dto.Notes;
+import com.org.utilities.Helper;
 
 @Repository
 public class NotesDao {
-	
+	EntityManagerFactory emf = Helper.getEmf();
+
 	public Notes fetchNotesById(int id) {
-		
+		EntityManager em = emf.createEntityManager();
+
+		Notes nt = em.find(Notes.class, id);
+		if (nt != null)
+			return nt;
 		return null;
 	}
+
 	public void deleteNotesByID(int id) {
-		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+
+		Notes nt = em.find(Notes.class, id);
+		if (nt != null) {
+			et.begin();
+			em.remove(nt);
+			et.commit();
+		} else
+			System.out.println("404 not found");
 	}
-	
+
 	public Notes fetchNotesByTitle(String title) {
+		EntityManager em = emf.createEntityManager();
+
+		Query q = em.createQuery("select e from Notes e where e.title = ?1");
+		q.setParameter(1, title);
+		List<Notes> rl = q.getResultList();
+		for (Notes n : rl)
+			return n;
 		return null;
 	}
 }
--- src/main/java/com/org/dao/UserDao.java
+++ src/main/java/com/org/dao/UserDao.java
@@ -1,8 +1,11 @@
 package com.org.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
@@ -11,24 +14,50 @@ import com.org.utilities.Helper;
 
 @Repository
 public class UserDao {
-	
+
 	EntityManagerFactory emf = Helper.getEmf();
-	
-	public void saveUser(User u) {
+
+	public void SaveandUpdateUser(User u) {
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction et = em.getTransaction();
-		
+
 		et.begin();
 		em.merge(u);
 		et.commit();
 	}
+
 	public User loginUser(String email, String pwd) {
+		EntityManager em = emf.createEntityManager();
+
+		Query q = em.createQuery("select u from User u where u.email = ?1");
+		q.setParameter(1, email);
+		List<User> rl = q.getResultList();
+		for (User usr : rl) {
+			if (usr.getPassword().equals(pwd))
+				return usr;
+		}
 		return null;
 	}
+
 	public void deleteUserById(int id) {
-		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+
+		User usr = em.find(User.class, id);
+		if (usr != null) {
+			et.begin();
+			em.remove(usr);
+			et.commit();
+		} else
+			System.out.println("404 Not found");
 	}
+
 	public User fetchUserById(int id) {
+		EntityManager em = emf.createEntityManager();
+
+		User usr = em.find(User.class, id);
+		if (usr != null)
+			return usr;
 		return null;
 	}
 }
